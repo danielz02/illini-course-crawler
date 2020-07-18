f20e8e7065f0fa03d2bb725b7a58ebec547f6001
danielz02 <31590926+danielz02@users.noreply.github.com>
2020-07-18 03:30:33 -0500

    Add parser for term information

--- src/parser.ts
+++ src/parser.ts
@@ -1,38 +1,43 @@
-import { writeFile } from 'fs';
-import fetch from 'node-fetch';
-import parser, {X2jOptions, X2jOptionsOptional} from "fast-xml-parser";
+import fetch from "node-fetch";
+import { TermDBRecord, Term, XMLRoot } from "./types"
+import parser, { X2jOptionsOptional } from "fast-xml-parser";
 
 const xmlParserOption: X2jOptionsOptional = {
     attributeNamePrefix: "",
     attrNodeName: false,
     ignoreAttributes: false,
+    parseAttributeValue: true,
     ignoreNameSpace: true,
+    parseNodeValue: true,
+    trimValues: true,
 };
 
-export const parseTerm = async () => {
+export const fetchYears = async () => {
     try {
         const termXml = await fetch("https://courses.illinois.edu/cisapp/explorer/schedule.xml?mode=summary");
         const termXmlStr  = await termXml.text();
-        console.log(termXmlStr);
         return parser.validate(termXmlStr) ? parser.parse(termXmlStr, xmlParserOption) : null;
     } catch(e) {
         console.error("Error while fetching documents", e);
     }
 }
 
-parseTerm()
-    .then(terms => {
-        terms.schedule.calendarYears.calendarYearSummary.forEach((year: {
-            id: number;
-            terms: {termDetail: any}; }) => {
-            console.log(year);
-            year.terms.termDetail.forEach((term: {id: string}) => {
-                console.log({
-                    CalendarYear: year.id,
-                    Term: term
-                })
-            })
-        })
-    })
-    .catch(err => console.error(err));
+export const parseYears = (years: XMLRoot): Array<TermDBRecord> => {
+    const calendarYearsArr = years.schedule.calendarYears.calendarYearSummary.map((year) => (
+        year.terms.termDetail instanceof Array ? year.terms.termDetail : year.terms.termDetail
+    ));
+    return calendarYearsArr.flat(1).map((term: Term) => (
+        {
+            TermId: term.id,
+            TermName: term.label,
+            TermDetailUrl: term.href,
+            CalendarYear: Number.parseInt(term.label.split(" ")[-1]),
+            PublicIndicator: term.publicIndicator === "Y",
+            ArchiveIndicator: term.archiveIndicator === "Y",
+            AttendingTerm: term.attendingTerm ? term.attendingTerm === "Y" : null,
+            DefaultTerm: term.defaultTerm ? term.defaultTerm === "Y" : null,
+            EnrollingTerm: term.enrollingTerm ? term.attendingTerm === "Y" : null
+        }
+    ));
+};
 
